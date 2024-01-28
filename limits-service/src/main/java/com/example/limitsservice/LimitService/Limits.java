1210ca8fa7acb948aab333de34f3232c4aeb6235
Johnnatan <romerozapatajonathan@gmail.com>
2024-01-28 15:33:22 -0500

    Configuration

---
+++ limits-service/src/main/java/com/example/limitsservice/LimitService/Limits.java
@@ -0,0 +1,16 @@
+package com.example.limitsservice.LimitService;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Limits {
+
+    private int minimum;
+    private int maximum;
+}
